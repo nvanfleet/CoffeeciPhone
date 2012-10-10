a52a8c3618f9e6b5ed86b5a47591358a6cf4e898
Nathan Van Fleet <nmcdavit@octopus.lan>
2012-10-09 22:15:01 -0400

    Trying to integrate coffee which seems to at least succeed in receiving a full string response.

--- Coffeed/coffeec.c
+++ Coffeed/coffeec.c
@@ -49,15 +49,15 @@ static int connectWithTimeout (int sfd, struct sockaddr *addr, int addrlen, stru
 
 void printhelp()
 {
-	fprintf(stderr, "-a IPADDRESS -p PORT -C COMMAND\n");
-	printf("COMMANDS\n------------------\n"
-		"CMD=SMODE          Steam mode activation\n"
-		"CMD=BMODE          Brew mode activation\n"
-		"CMD=WAKE           wake up PID\n"
-		"CMD=SLEEP          put PID to sleep\n"
-		"CMD=SDOWN          Shutdown entire daemon\n"
-		"CMD=STATUS         Print server status\n"
+	fprintf(stderr, "-a IPADDRESS -p PORT -c COMMAND\n");
+	printf(
+		"Coffeec Version 2.0"
 		"SETTING\n------------------\n"
+		"VERSION            Get the version of coffeed\n"
+		"SHUTD              Shutdown the system (stops daemon)\n"
+		"BMODE=<TRUE/FALSE> Set to brewmode or steam mode\n"
+		"SLEEP=<TRUE/FALSE> Set to sleep or wake\n"
+		"TPOINT             Get the current temperature (read only)\n"
 		"BPOINT=<float>     Set Brew setpoint\n"
 		"SPOINT=<float>     Set Steam setpoint\n"
 		"PGAIN=<float>      Set PID p-gain\n"
@@ -68,89 +68,59 @@ void printhelp()
 		);
 }
 
-int main(int argc, char **argv)
+int sendMessage(char *addr, int port, char *command, char *buffer, int bsize)
 {
-    int abort = 0;
-    int port;
-	in_addr_t address;
-    char *command;
-    
     ssize_t z;
     int com_socket;
     struct sockaddr_in server_address;
     struct timeval timeout;
-
-    int opt;
-    
-    //Defaults
-    address = inet_addr("127.0.0.1");
-    port = 4949;
-    
-	//INCLUDED ARGUMENTS FROM CLI
-	while((opt = getopt(argc, argv, "a:p:c:")) > 0) 
-	{
-		switch(opt)
-		{
-            case 'a':
-				address = inet_addr(optarg);
-				break;
-            case 'p':
-				port = atoi(optarg);
-                break;
-            case 'c':
-				abort = 1;
-				command = (char *)optarg;
-				break;   
-            default:
-		printhelp();
-		exit(0);
-        }
-    }
+	in_addr_t address = inet_addr(addr);
+	
     // Server
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = address;
-
+	
     if (server_address.sin_addr.s_addr == INADDR_NONE)
         fprintf(stderr, "Server address failed\n");
-
+	
     if(!abort)
     {
         fprintf(stderr, "No Command given\n");
-	printhelp();
+		printhelp();
         exit(0);
     }
     else
     {
     	printf("Address %s Port %d\n", inet_ntoa(server_address.sin_addr), port);
     }
-
+	
     // Create com_socket
     com_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (com_socket == -1)
-         fprintf(stderr, "Socket failed\n");
-
+		fprintf(stderr, "Socket failed\n");
+	
     /*
-    // Client
-    struct sockaddr_in client_address;
-    memset(&client_address,0,sizeof client_address);
-    client_address.sin_family = AF_INET;
-    client_address.sin_port = 0;
-    client_address.sin_addr.s_addr = ntohl(INADDR_ANY);
+	 // Client
+	 struct sockaddr_in client_address;
+	 memset(&client_address,0,sizeof client_address);
+	 client_address.sin_family = AF_INET;
+	 client_address.sin_port = 0;
+	 client_address.sin_addr.s_addr = ntohl(INADDR_ANY);
+	 
+	 if (client_address.sin_addr.s_addr == INADDR_NONE)
+	 fprintf(stderr, "Client address failed\n");
+	 
+	 // Bind
+	 z= bind(com_socket, (struct sockaddr *)&client_address, sizeof (client_address));
+	 if ( z == -1 )
+	 fprintf(stderr,"Binding port\n");
+	 */
     
-    if (client_address.sin_addr.s_addr == INADDR_NONE)
-        fprintf(stderr, "Client address failed\n");
-    
-    // Bind
-    z= bind(com_socket, (struct sockaddr *)&client_address, sizeof (client_address));
-    if ( z == -1 )
-        fprintf(stderr,"Binding port\n");
-    */
-    
-    timeout.tv_sec = 2; /* 2 seconds */ 
+    timeout.tv_sec = 2; /* 2 seconds */
     timeout.tv_usec = 0; /* + 0 usec */
-
+    
     // Connect
     //z = connectWithTimeout(com_socket, (struct sockaddr *) &server_address, len_inet, &timeout);
     z = connect(com_socket, (struct sockaddr *) &server_address, sizeof(server_address));
@@ -168,36 +138,74 @@ int main(int argc, char **argv)
         
         fprintf(stderr, "Connect failed\n");
     }
-    else 
-	printf("Connect Success\n");
-
-	int sSize = 256;
-	char sbuf[sSize];
-
-	z = snprintf(sbuf, sSize, command);
-        snprintf(&sbuf[z], sSize-z, "\0");
-
+    else
+		printf("Connect Success\n");
+	
+	z = snprintf(buffer, bsize, &command[0]);
+//	snprintf(&buffer[z], bsize-z, "\0");
+	
     // SEND
-    z = send(com_socket, sbuf, strlen(sbuf), 0);
+    z = send(com_socket, buffer, strlen(buffer), 0);
     if (z < 0)
         fprintf(stderr,"send failure\n");
     else
-	printf("Send Succeeded\n");
+		printf("Send Succeeded\n");
     
     // READ
-    z = recv(com_socket, sbuf, sSize, 0);
+    z = recv(com_socket, buffer, bsize, 0);
     if (z < 0)
         fprintf(stderr,"receive failure\n");
     else
-	printf("Receive Succeeded\n");
+		printf("Receive Succeeded\n");
     
-	snprintf(&sbuf[z], sSize-z,"\0");
-
+//	snprintf(&buffer[z], bsize-z,"\0");
+	
     // Output
-    printf("Received Response: %s\n", sbuf);
+    printf("Received Response: %s\n", buffer);
     
     close(com_socket);
+	
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	int buffersize = 256;
+	char buffer[buffersize];
+	
+    int opt;
+	int port;
+	int abort = 0;
+	char *command;
+	char *addr;
+	
+    //Defaults
+    addr = "127.0.0.1";
+    port = 4949;
     
+	//INCLUDED ARGUMENTS FROM CLI
+	while((opt = getopt(argc, argv, "a:p:c:")) > 0) 
+	{
+		switch(opt)
+		{
+            case 'a':
+				addr = (char *)optarg;
+				break;
+            case 'p':
+				port = atoi(optarg);
+                break;
+            case 'c':
+				abort = 1;
+				command = (char *)optarg;
+				break;   
+            default:
+		printhelp();
+		exit(0);
+        }
+    }
+	
+	sendMessage(addr, port, command, buffer, buffersize);
+	    
     exit(1);
 }
 
---
+++ Coffeed/coffeec.h
@@ -0,0 +1,5 @@
+/* ------------------------------------------------------------------------- */
+/*       coffeedc -- A PID system for espresso machines                       */
+/* ------------------------------------------------------------------------- */
+
+int sendMessage(char *address, int port, char *command, char *buffer, int bsize);
