f72504900d206b0bbaea9a5f83b41cef21e6ad53
Nathan Van Fleet <nathan.vanfleet@gmail.com>
2013-08-16 10:57:59 -0400

    changed files

--- Coffeed/TempViewController.m
+++ Coffeed/TempViewController.m
@@ -1,3 +1,7 @@
+// Copyright (C) 2013 Nathan Van Fleet
+//
+// This is free software, licensed under the GNU General Public License v2.
+// See /LICENSE for more information.
 //
 //  TempViewController.m
 //  Coffeed
